682d6c46f316282c48d82de8145b936ef2d0b7b7
fedco-gtz <gutierrezfedericog@gmail.com>
2025-03-15 12:58:29 -0300

    Actualización 15/03

--- js/BaEducacion.js
+++ js/BaEducacion.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("Coderhouse.json")
+    fetch("json/BaEducacion.json")
         .then(response => response.json())
         .then(data => {
             let tbody = document.querySelector("#data-table tbody");
